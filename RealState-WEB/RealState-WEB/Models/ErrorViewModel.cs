a509a58bbb21e8d7ae1d7b0ecb9fff86f1088120
iLucaru <luis98ccc@gmail.com>
2023-06-12 21:15:46 -0600

    Creación del proyecto WEB
    Co-Authored-By: machoquiros <12601040+machoquiros@users.noreply.github.com>
    Co-Authored-By: hillaryfide21 <105746891+hillaryfide21@users.noreply.github.com>

---
+++ RealState-WEB/RealState-WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RealState_WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
